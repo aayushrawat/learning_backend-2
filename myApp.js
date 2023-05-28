92387429e19cb61cb77e1e6fe4139e5700ebb309
aayushrawat <aayushrawatmain@gmail.com>
2023-05-28 15:53:35 +0530

    sending html files

--- myApp.js
+++ myApp.js
@@ -3,9 +3,10 @@ let app = express();
 
 console.log("Hello World");
 
-
+const absolute_path = __dirname + '/views/index.html';
 app.get("/", function(req, res) {
-  res.send("Hello Express");
+//   res.send("Hello Express");
+  res.sendFile(absolute_path);
 });
 
 
