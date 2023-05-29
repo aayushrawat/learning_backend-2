b26e30f7b358b3c4e9ec5c1e6dfa357358f363e6
aayushrawat <aayushrawatmain@gmail.com>
2023-05-29 11:33:40 +0530

    json added

--- myApp.js
+++ myApp.js
@@ -4,6 +4,8 @@ let app = express();
 console.log("Hello World");
 
 const absolute_path = __dirname + '/views/index.html';
+
+
 app.get("/", function(req, res) {
 //   res.send("Hello Express");
   res.sendFile(absolute_path);
@@ -11,6 +13,9 @@ app.get("/", function(req, res) {
 
 app.use("/public", express.static(__dirname + "/public"));
 
+app.get("/json", function(req, res){
+    res.json({"message": "Hello json"})
+});
 
 
 
